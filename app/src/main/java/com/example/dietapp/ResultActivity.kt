e8657931c94aae0e287f47411183b9c88da9b135
Yunsol Shin <ys.2871@daum.net>
2022-01-21 02:08:01 +0900

    윤솔 2022/01/21 오전 02:07 카톡방에 보낸 부분까지 완료 오류도 해결완료

---
+++ app/src/main/java/com/example/dietapp/ResultActivity.kt
@@ -0,0 +1,81 @@
+package com.example.dietapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.ImageView
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+
+
+
+class ResultActivity : AppCompatActivity() {
+
+    lateinit var ResultTextView : TextView
+    lateinit var imageView : ImageView
+    lateinit var radioGroup: RadioGroup
+    lateinit var radioButton1: RadioButton
+    lateinit var radioButton2: RadioButton
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bmi_result)
+
+        ResultTextView = findViewById(R.id.bmiResultTextView)
+        imageView = findViewById<ImageView>(R.id.imageView)
+        radioGroup = findViewById(R.id.RadioGroup)
+        radioButton1 = findViewById(R.id.radioButton2)
+        radioButton2 = findViewById(R.id.radioButton1)
+
+
+
+        var height = intent.getStringExtra("height").toInt()
+        var weight = intent.getStringExtra("weight").toInt()
+
+
+        //BMI 계산
+        var bmi = weight / Math.pow(height/100.0, 2.0)
+        //글자로 출력
+        when {
+            bmi >= 35 -> ResultTextView.text = "고도 비만"
+            bmi >= 30 -> ResultTextView.text = "2단계 비만"
+            bmi >= 25 -> ResultTextView.text = "1단계 비만"
+            bmi >= 23 -> ResultTextView.text = "과체중"
+            bmi >= 18.5 -> ResultTextView.text = "정상"
+            else -> ResultTextView.text = "저체중"
+        }
+
+        //이미지로 출력
+        when{
+            bmi >= 23 ->
+                imageView.setImageResource(
+                    R.drawable.ic_baseline_sentiment_very_dissatisfied_24)
+            bmi > 18.5 ->
+                imageView.setImageResource(
+                    R.drawable.ic_baseline_sentiment_satisfied_alt_24)
+            else ->
+                imageView.setImageResource(
+                    R.drawable.ic_baseline_mood_bad_24)
+        }
+
+        when{
+            bmi >= 23 -> {
+                radioButton1.text = "살 빼기"
+                radioButton2.text = "유지하기"
+            }
+            bmi > 18.5 -> {
+                radioButton1.text = "유지하기"
+                radioButton2.visibility = View.INVISIBLE
+            }
+            else -> {
+                radioButton1.text = "살 찌기"
+                radioButton2.text = "유지하기"
+            }
+        }
+
+        Toast.makeText(this,  "$bmi", Toast.LENGTH_SHORT).show()
+    }
+}
