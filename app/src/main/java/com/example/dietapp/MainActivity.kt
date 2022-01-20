6e1b026f18e60b0fc2662846dd34be86e97e44f7
ssh09015 <ssh09015_@naver.com>
2022-01-20 23:56:51 +0900

    추천창 구성

--- app/src/main/java/com/example/dietapp/MainActivity.kt
+++ app/src/main/java/com/example/dietapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.dietapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recobutton:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recobutton=findViewById(R.id.recobutton)
+
+        recobutton.setOnClickListener {
+            var intent=Intent(this, reco_op1::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/dietapp/reco_op2.kt
@@ -0,0 +1,11 @@
+package com.example.dietapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class reco_op2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reco_op2)
+    }
+}
