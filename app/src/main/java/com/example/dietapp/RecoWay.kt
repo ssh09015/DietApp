43c2db4d8addd00b7a6f1e3d67121fe823f346e7
Lee-Song-Ha <84578843+Lee-Song-Ha@users.noreply.github.com>
2022-01-23 16:45:18 +0900

    송하_bmi별로 운동 추천 변경

--- app/src/main/java/com/example/dietapp/RecoWay.kt
+++ app/src/main/java/com/example/dietapp/RecoWay.kt
@@ -5,12 +5,18 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.TextView
 
 class RecoWay : AppCompatActivity() {
 
     lateinit var video1 : ImageButton
     lateinit var video2 : ImageButton
     lateinit var video3 : ImageButton
+    lateinit var text1: TextView
+    lateinit var text2: TextView
+    lateinit var text3: TextView
+    lateinit var text4: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,24 +25,152 @@ class RecoWay : AppCompatActivity() {
         video1 = findViewById(R.id.imageButton)
         video2 = findViewById(R.id.imageButton1)
         video3 = findViewById(R.id.imageButton2)
+        text1 = findViewById(R.id.textView4)
+        text2 = findViewById(R.id.textView2)
+        text3 = findViewById(R.id.textView5)
+        text4 = findViewById(R.id.textView6)
+
+        // ResultActivity에서 bmi 값 받기 (송하)
+        var bmi = intent.getDoubleExtra("bmi", 0.0)
 
         // 추천 운동에서 동영상 부분을 누르면 추천 운동 영상의 유튜브 링크로 이동 (송하)
-        video1.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse("https://youtu.be/PRGLzLU3XCc")
-            startActivity(Intent.createChooser(intent, null))
-        }
+        when {
+            // 정상 (유지로 적음)
+            bmi >= 18.5 -> {
+                text1.text = "과식하지 않기\n" + "물 많이 먹기"
+                text2.text = "유산소 (소미핏)"
+                text3.text = "운동 전 스트레칭 (땅끄부부)"
+                text4.visibility = android.view.View.INVISIBLE
+                video3.visibility = android.view.View.INVISIBLE
 
-        video2.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse("https://youtu.be/ahbAnkN4KJ0")
-            startActivity(Intent.createChooser(intent, null))
-        }
+                video1.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/lazFuEUBB7A")
+                    startActivity(Intent.createChooser(intent, null))
+                }
 
-        video3.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse("https://youtu.be/jfr6MtMB89A")
-            startActivity(Intent.createChooser(intent, null))
+                video2.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/2LyDkE7sDec")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+
+            }
+            // 과체중 (감량으로 적음)
+            bmi >= 23 -> {
+                text1.text = "야식, 간식 줄이기\n" + "과식하지 않기\n" + "물 많이 먹기\n" + "식사 시간에 채소 먹는 양 늘리기"
+                text2.text = "유산소 (빅씨스)"
+                text3.text = "전신운동 (힘으뜸)"
+                text4.text = "코어운동 (힘으뜸)"
+
+                video1.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/sTX0C08SYBM")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+
+                video2.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/W8JpE-sdUAs")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+
+                video3.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/hJuO1AUqLUc")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+            }
+            // 1단계 비만
+            bmi >= 25 -> {
+                text1.text = "야식, 간식 줄이기\n" + "식사 시간에 채소 먹는 양 늘리기\n" + "물 많이 먹기"
+                text2.text = "유산소 (빅씨스)"
+                text3.text = "전신 스트레칭 (제이제이샬롱드핏)"
+                text4.visibility = android.view.View.INVISIBLE
+                video3.visibility = android.view.View.INVISIBLE
+
+                video1.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/0IWibGOf1jU")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+
+                video2.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/sWCbpXfoU5A")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+            }
+            // 2단계 비만
+            bmi >= 30 -> {
+                text1.text = "야식, 간식 줄이기\n" + "식사 시간에 채소 먹는 양 늘리기\n" + "물 많이 먹기"
+                text2.text = "유산소 (힘으뜸)"
+                text3.text = "걷기 유산소 (땅끄부부)"
+                text4.text = "전신 스트레칭 (DanoTV)"
+
+                video1.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/GQ_Dt7_Jfk8")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+
+                video2.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/t70t-sklypk")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+
+                video3.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/WwVLRtqGmzI")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+            }
+            // 3단계 비만
+            bmi >= 35 -> {
+                text1.text = "야식, 간식 줄이기\n" + "식사 시간에 채소 먹는 양 늘리기\n" + "물 많이 먹기"
+                text2.text = "유산소 (빅씨스)"
+                text3.text = "전신 스트레칭 (DanoTV)"
+                text4.visibility = android.view.View.INVISIBLE
+                video3.visibility = android.view.View.INVISIBLE
+
+                video1.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/myNjmnvI6x0")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+
+                video2.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/WwVLRtqGmzI")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+            }
+            // 저체중
+            else -> {
+                text1.text = "하루 세번 규칙적으로 식사하기\n물 많이 먹기"
+                text2.text = "유산소 (빅씨스)"
+                text3.text = "운동 전 스트레칭 (소미핏)"
+                text4.text = "타바타운동 (핏블리)"
+
+                video1.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/PRGLzLU3XCc")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+
+                video2.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/ahbAnkN4KJ0")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+
+                video3.setOnClickListener {
+                    val intent = Intent(Intent.ACTION_VIEW)
+                    intent.data = Uri.parse("https://youtu.be/jfr6MtMB89A")
+                    startActivity(Intent.createChooser(intent, null))
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/dietapp/ResultActivity.kt
+++ app/src/main/java/com/example/dietapp/ResultActivity.kt
@@ -35,10 +35,11 @@ class ResultActivity : AppCompatActivity() {
 
         var height = intent.getStringExtra("height").toInt()
         var weight = intent.getStringExtra("weight").toInt()
+        var num : Int
 
 
-        //BMI 계산
-        var bmi = weight / Math.pow(height/100.0, 2.0)
+        //BMI 계산_변수형 Double로 변경 (송하)
+        var bmi : Double = weight / Math.pow(height/100.0, 2.0)
 
         //글자로 출력
         when {
@@ -84,11 +85,14 @@ class ResultActivity : AppCompatActivity() {
             when(checkedId){
                 R.id.radioButton1 -> {
                     val intent = Intent(this, RecoWay::class.java)
+                    // bmi 값을 RecoWay로 전달 (송하)
+                    intent.putExtra("bmi", bmi)
                     startActivity(intent)
                 }
 
                 R.id.radioButton2 -> {
                     val intent = Intent(this, RecoWay::class.java)
+                    intent.putExtra("bmi", bmi)
                     startActivity(intent)
                 }
             }
