52ab0373f676d9ff5a01aad74d124b4a72b0ec99
Lee-Song-Ha <84578843+Lee-Song-Ha@users.noreply.github.com>
2022-01-31 19:22:24 +0900

    이송하_회원정보 창에 툴바 추가

--- app/src/main/java/com/example/dietapp/UserInfoActivity.kt
+++ app/src/main/java/com/example/dietapp/UserInfoActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.core.view.GravityCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
@@ -25,6 +27,16 @@ class UserInfoActivity : AppCompatActivity() {
         birthDayTextView=findViewById(R.id.birthDayTextView)
         addressTextView=findViewById(R.id.addressTextView)
 
+        val toolbar : androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar)
+
+        // 툴바를 액티비티의 앱바로 지정 (송하)
+        setSupportActionBar(toolbar)
+        // 드로어를 꺼낼 홈 버튼 활성화 (송하)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        // 홈버튼 (메뉴모양버튼으로) 이미지 변경 (송하)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
+        // 툴바에 타이틀 안보이게 (송하)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
         // 파이어베이스에 저장된 사용자 정보 불러오기 (파이어베이스 문서 참조)
         val user = FirebaseAuth.getInstance().currentUser
@@ -48,6 +60,19 @@ class UserInfoActivity : AppCompatActivity() {
                 Log.d(MainActivity.TAG, "get failed with ", task.exception)
             }
         }
+    }
+
+    // 메뉴바 누르면 네비게이션 기능 나오게 하는 함수 (송하)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item!!.itemId){
+            android.R.id.home -> { // 메뉴 버튼
+                var intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     private fun myStartActivity(c: Class<*>) { // 인텐트 이동 함수 따로 만듦듦
